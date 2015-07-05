971fc50fb99dc845b037c12fb7cfe3d65b5e838e
willzyx <willzyx@nomail.com>
2015-07-06 02:02:06 +0300

    minor refactoring

--- src/main/util_listing.php
+++ src/main/util_listing.php
@@ -4,70 +4,77 @@ require_once 'proto_model.php';
 
 function reportListing($db, $authInfo)
 {
+    function writeError()
+    {
+        ?>
+        <div class="alert alert-danger" role="alert">Can't connect to database</div>
+    <?php
+    }
+
     $sql = $db ->prepare(
         "SELECT data_order
           FROM t_orders
           WHERE perform_uid IS NULL
           ORDER BY order_id DESC"
     );
-    if ($sql) {
-        if ($sql ->execute()) {
+    if (!$sql) writeError();
+    else {
 
-            function writeTableHead($authInfo)
-            {
-                ?>
-                <table class="table table-striped table-listing">
-                <thead>
-                <tr>
-                    <th class="td-desc">Description</th>
-                    <th class="td-price">Price</th>
-                    <?php if ($authInfo ->isConsumer()) { ?>
-                        <th></th>
-                    <?php } ?>
-                </tr>
-                </thead>
-                <tbody>
-            <?php
-            }
+        function writeTableHead($authInfo)
+        {
+            ?>
+            <table class="table table-striped table-listing">
+            <thead>
+            <tr>
+                <th class="td-desc">Description</th>
+                <th class="td-price">Price</th>
+                <?php if ($authInfo ->isConsumer()) { ?>
+                    <th></th>
+                <?php } ?>
+            </tr>
+            </thead>
+            <tbody>
+        <?php
+        }
 
-            function writeTableTail()
-            {
-                ?>
-                </tbody>
-                </table>
-            <?php
-            }
+        function writeTableTail()
+        {
+            ?>
+            </tbody>
+            </table>
+        <?php
+        }
 
-            function writeEmptyTable()
-            {
-                ?>
-                <div class="alert alert-info" role="alert">No orders found</div>
-            <?php
-            }
+        function writeEmptyTable()
+        {
+            ?>
+            <div class="alert alert-info" role="alert">No orders found</div>
+        <?php
+        }
 
-            function writeTableRow($authInfo, $orderInfo)
-            {
-                ?>
+        function writeTableRow($authInfo, $orderInfo)
+        {
+            ?>
 
-                <tr id="order_<?php echo $orderInfo->getId(); ?>">
-                    <td class="td-desc"><?php echo $orderInfo->getDescription(); ?> </td>
-                    <td class="td-price">
-                        <?php echo $orderInfo->getPerformCost(); ?> nail(s)
+            <tr id="order_<?php echo $orderInfo->getId(); ?>">
+                <td class="td-desc"><?php echo $orderInfo->getDescription(); ?></td>
+                <td class="td-price">
+                    <?php echo $orderInfo->getPerformCost(); ?> nail(s)
+                </td>
+                <?php if ($authInfo ->isConsumer()) { ?>
+                    <td>
+                        <button type="button" class="btn btn-primary"
+                                onclick="consumeOrder('<?php echo $orderInfo->getId(); ?>')">
+                            Buy it!
+                        </button>
                     </td>
-                    <?php if ($authInfo ->isConsumer()) { ?>
-                        <td>
-                            <button type="button" class="btn btn-primary"
-                                    onclick="consumeOrder('<?php echo $orderInfo->getId(); ?>')">
-                                Buy it!
-                            </button>
-                        </td>
-                    <?php } ?>
-                </tr>
-            <?php
-            }
+                <?php } ?>
+            </tr>
+        <?php
+        }
 
+        if ($sql ->execute() && $sql ->bind_result($data_order)) {
             $firstLine = true;
-            $sql ->bind_result($data_order);
             while ($sql ->fetch()) {
                 if ($firstLine) {
                     writeTableHead($authInfo);
@@ -79,7 +86,7 @@ function reportListing($db, $authInfo)
             }
             if ($firstLine) writeEmptyTable();
             else writeTableTail();
-        }
+        } else writeError();
         $sql ->close();
     }
 
