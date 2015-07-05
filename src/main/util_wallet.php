56a868ec5a0f8a898360257a82bc1d89c95d3fe6
willzyx <willzyx@nomail.com>
2015-07-06 01:45:04 +0300

    minor refactoring

--- src/main/util_wallet.php
+++ src/main/util_wallet.php
@@ -17,42 +17,40 @@ function doArrangeOrders($db, $user)
     $sql = $db ->prepare(
         "SET @i = (SELECT IFNULL(MAX(perform_order), 0) FROM t_orders WHERE perform_uid = ?)"
     );
-    if (!$sql) return false;
-    if (!$sql ->bind_param("s", $user)) return false;
-    if (!$sql ->execute()) return false;
+    $fl = $sql && $sql ->bind_param("s", $user) && $sql ->execute();
+    $fl = $sql && $sql ->close() && $fl;
+    if (!$fl) return false;
     $sql = $db ->prepare(
         "UPDATE t_orders
           SET perform_order = (@i:=@i+1)
           WHERE perform_uid = ? AND perform_order IS NULL"
     );
-    if (!$sql) return false;
-    if (!$sql ->bind_param("s", $user)) return false;
-    if (!$sql ->execute()) return false;
+    $fl = $sql && $sql ->bind_param("s", $user) && $sql ->execute();
+    $fl = $sql && $sql ->close() && $fl;
+    if (!$fl) return false;
     return true;
 }
 
-function getWallet($db, $user)
+function findWallet($db, $user)
 {
-    $wallet = null;
     $sql = $db ->prepare("SELECT data_wallet FROM t_users WHERE uid = ?");
-    $sql ->bind_param("s", $user);
-    if ($sql ->execute()) {
+    if (!$sql) return null;
+    $wallet = null;
+    if ($sql ->bind_param("s", $user) && $sql ->execute()) {
         $sql ->bind_result($dataWallet);
+        $wallet = new UserWalletInfo();
         if ($sql->fetch()) {
-            $wallet = new UserWalletInfo();
             $wallet ->parseFromString($dataWallet);
+        } else {
+            $wallet ->setQuantityOfMoney(0);
+            $wallet ->setPerformOrder(0);
         }
     }
     $sql ->close();
-    if (!$wallet) {
-        $wallet = new UserWalletInfo();
-        $wallet ->setQuantityOfMoney(0);
-        $wallet ->setPerformOrder(0);
-    }
     return $wallet;
 }
 
-function putWallet($db, $user, $wallet)
+function saveWallet($db, $user, $wallet)
 {
     $sql = $db ->prepare(
         "INSERT INTO t_users (uid, data_wallet) VALUES (?, ?)
@@ -60,35 +58,38 @@ function putWallet($db, $user, $wallet)
     );
     if ($sql) {
         $data_wallet = $wallet ->serializeToString();
-        if ($sql ->bind_param("sss", $user, $data_wallet, $data_wallet)) {
-            $sql ->execute();
-            $sql ->close();
-        }
+        $sql ->bind_param("sss", $user, $data_wallet, $data_wallet) && $sql ->execute();
+        $sql ->close();
     }
 }
 
 function refreshWallet($db, $user)
 {
     doArrangeOrders($db, $user);
-    $wallet = getWallet($db, $user);
-    $valuePerformOrder = $wallet ->getPerformOrder();
+    $wallet = findWallet($db, $user);
+    if (!$wallet) return null;
+    $valueOrder = $wallet ->getPerformOrder();
     $valueMoney = $wallet ->getQuantityOfMoney();
     $sql = $db ->prepare("SELECT data_perform, perform_order FROM t_orders WHERE perform_uid = ? AND perform_order > ?");
-    $sql ->bind_param("si", $user, $valuePerformOrder);
-    if ($sql ->execute()) {
-        $sql ->bind_result($data_perform, $perform_order);
-        while ($sql ->fetch()) {
-            if ($valuePerformOrder < $perform_order) {
-                $valuePerformOrder = $perform_order;
+    if ($sql) {
+        $fl = $sql ->bind_param("si", $user, $valueOrder) && $sql ->execute();
+        if ($fl) {
+            $fl = $sql ->bind_result($data_perform, $perform_order);
+            if ($fl) {
+                while ($sql ->fetch()) {
+                    if ($valueOrder < $perform_order) {
+                        $valueOrder = $perform_order;
+                    }
+                    $infoPerform = new OrderPerformInfo();
+                    $infoPerform ->parseFromString($data_perform);
+                    $valueMoney += $infoPerform ->getPerformProfit();
+                }
             }
-            $infoPerform = new OrderPerformInfo();
-            $infoPerform ->parseFromString($data_perform);
-            $valueMoney += $infoPerform ->getPerformProfit();
         }
-        if ($sql ->close()) {
-            $wallet ->setPerformOrder($valuePerformOrder);
+        if ($sql ->close() && $fl) {
+            $wallet ->setPerformOrder($valueOrder);
             $wallet ->setQuantityOfMoney($valueMoney);
-            putWallet($db, $user, $wallet);
+            saveWallet($db, $user, $wallet);
         }
     }
     return $wallet;
